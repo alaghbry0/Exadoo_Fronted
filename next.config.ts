38138c775e283a2368d8d71e5ffdc5820f6faa3f
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-02-01 22:11:49 +0300

    تحديثات جديدة للتطبيق

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["api.telegram.org"], // ✅ السماح بتحميل الصور من Telegram API
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.resolve.fallback = { fs: false };
