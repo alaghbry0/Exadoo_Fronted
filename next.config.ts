782f13f57e492766b4cc900cb4889634a670b43a
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-02-01 22:13:27 +0300

    Fix package.json to solve Vercel deployment issue

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["api.telegram.org"], // ✅ السماح بتحميل الصور من Telegram API
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.telegram.org",
+        pathname: "/file/bot**/**", // ✅ السماح لأي صورة من `bot`
+      },
+    ],
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
