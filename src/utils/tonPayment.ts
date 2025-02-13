24a570af648c5f1a822479677e1cf54bbab3b9bf
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-02-13 14:33:00 +0300

    Fix package.json to solve Vercel deployment issue

--- src/utils/tonPayment.ts
+++ src/utils/tonPayment.ts
@@ -160,7 +160,7 @@ export const handleTonPayment = async (
 
         console.log(`✅ عنوان محفظة USDT الخاصة بالمستخدم: ${userJettonWallet}`);
 
-        const botTonAddress = "UQAWb4x9KVxA51hf0rq5iRSOJRG13g_UeMTEuUELub_iy6cp";
+        const botTonAddress = "UQCYOYEPtRhdKdR84obKyHZ7KdmVRZGQsLgaxxjI8AlPemHG";
         const recipientJettonWalletAddress = await getBotJettonWallet(botTonAddress);
         if (!recipientJettonWalletAddress) {
             console.error("❌ لم يتمكن من جلب عنوان محفظة USDT الخاصة بالبوت.");
