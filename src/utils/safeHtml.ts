4c596597551bf52fd5494abbf53770493442cb8a
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-03-06 02:40:05 +0300

    Fix package.json to solve Vercel deployment issue

--- src/utils/safeHtml.ts
+++ src/utils/safeHtml.ts
@@ -1,6 +1,10 @@
 // utils/safeHtml.ts
 import DOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const { window } = new JSDOM('');
+const domPurify = DOMPurify(window);
 
 export const sanitizeHtml = (html: string) => ({
-  __html: DOMPurify.sanitize(html)
+  __html: domPurify.sanitize(html)
 });
