248c8872bf0dcd94a050e9a410d08c96846417f4
Mohammed Alaghbry <mohammedalaghbry3@gmail.com>
2025-03-22 07:59:22 +0300

    Fix package.json to solve Vercel deployment issue

--- src/components/SubscriptionModal.tsx
+++ src/components/SubscriptionModal.tsx
@@ -151,7 +151,7 @@ const SubscriptionModal = ({ plan, onClose }: { plan: SubscriptionPlan | null; o
             if (data.invite_link) {
               window.dispatchEvent(
                 new CustomEvent('subscription_update', {
-                  detail: { invite_link: data.invite_link, formatted_message: data.formatted_message }
+                  detail: { invite_link: data.invite_link, formatted_message: data.fmessage }
                 })
               )
             }
